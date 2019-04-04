53d2fca7c3f9f33360920a6405414e353470b851
DannyBoyNYC <daniel.deverell@gmail.com>
2019-04-04 16:21:31 -0400

    changed sidebar content

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      single: true,
+      single: false,
       multiData: multiData,
       singleData: singleData
     };
--- src/sidebar/components/EqMultiCompanyData.js
+++ src/sidebar/components/EqMultiCompanyData.js
@@ -46,66 +46,136 @@ class MultiCompanyData extends Component {
             </p>
             <p className='status'>Unchanged</p>
           </li>
+          {/* hiden for industry view */}
+          <div className='sub-stats' ref='stats'>
+            <ul>
+          <li>
+            <p class='sector'>Industry View</p>
+            <p class='sector-rating'>
+              <a href='#0'>NEUTRAL</a>
+            </p>
+            <p class='status'>Unchanged</p>
+              </li>
+            </ul>
+            </div>
           {/* hidden activated by link above */}
           <div className='sub-stats' ref='stats'>
-            <table>
-              <thead>
-                <tr>
-                  <th colSpan='2'>Rating</th>
-                  <th>Price</th>
-                  <th colSpan='3'>Price Target</th>
-                </tr>
-              </thead>
-              <thead>
-                <tr>
-                  <th>Old</th>
-                  <th>New</th>
-                  <th>28 Mar 19</th>
-                  <th>Old</th>
-                  <th>New</th>
-                  <th>%Chg</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>Neutral</td>
-                  <td>Neutral</td>
-                  <td>0.63</td>
-                  <td>1.00</td>
-                  <td>1.00</td>
-                  <td>1.00</td>
-                </tr>
-              </tbody>
-            </table>
+            <ul>
+          <li>
+            <p class='sector'>Industry View</p>
+            <p class='sector-rating'>
+              <a href='#0'>NEUTRAL</a>
+            </p>
+            <p class='status'>Unchanged</p>
+          </li>
 
-            <table>
-              <thead>
-                <tr>
-                  <th colSpan='3'>EPS FY1 (E)</th>
-                  <th colSpan='3'>EPS FY2 (E)</th>
-                </tr>
-              </thead>
-              <thead>
-                <tr>
-                  <th>Old</th>
-                  <th>New</th>
-                  <th>%Chg</th>
-                  <th>Old</th>
-                  <th>New</th>
-                  <th>%Chg</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>1.00</td>
-                  <td>1.00</td>
-                  <td>1.00</td>
-                  <td>1.00</td>
-                  <td>1.00</td>
-                  <td>1.00</td>
-                </tr>
-              </tbody>
-            </table>
+          <li>
+            <p class='sector'>Price Target</p>
+            <p class='sector-rating'>
+              <a href='#0'>USD 51.00</a>
+            </p>
+            <p class='status'>Raised 4% from USD 49.00</p>
+          </li>
+          {/* end 3 */}
+          <li>
+            <p class='sector'>Price (23-Jan-2019)</p>
+            <p class='sector-rating'>
+              <a href='#0'>USD 48.34</a>
+            </p>
+          </li>
+
+          <li>
+            <p class='sector'>Potential Upside/Downside</p>
+            <p class='sector-rating'>
+              <a href='#0'>+6.2%</a>
+            </p>
+          </li>
+
+          <li>
+            <p class='sector'>Tickers</p>
+            <p class='sector-rating'>
+              <a href='#0'>MDLZ</a>
+            </p>
+          </li>
+          {/* end 6 */}
+          <li>
+            <p class='sector'>Market Cap (USD mn)</p>
+            <p class='sector-rating'>
+              <a href='#0'>69378</a>
+            </p>
+          </li>
+
+          <li>
+            <p class='sector'>Shares Outstanding (mn)</p>
+            <p class='sector-rating'>
+              <a href='#0'>1444.17</a>
+            </p>
+          </li>
+
+          <li>
+            <p class='sector'>Free Float (%)</p>
+            <p class='sector-rating'>
+              <a href='#0'>99.65</a>
+            </p>
+          </li>
+
+          <li>
+            <p class='sector'>52 Wk Avg Daily Volume (mn)</p>
+            <p class='sector-rating'>
+              <a href='#0'>7.9</a>
+            </p>
+          </li>
+
+          <li>
+            <p class='sector'>52 Wk Avg Daily Value (USD mn)</p>
+            <p class='sector-rating'>
+              <a href='#0'>N/A</a>
+            </p>
+          </li>
+
+          <li>
+            <p class='sector'>Dividend Yield (%)</p>
+            <p class='sector-rating'>
+              <a href='#0'>2.2</a>
+            </p>
+          </li>
+          {/* end 12 */}
+          <li>
+            <p class='sector'>Return on Equity TTM (%)</p>
+            <p class='sector-rating'>
+              <a href='#0'>2.2</a>
+            </p>
+          </li>
+          <li>
+            <p class='sector'>Current BVPS (USD)</p>
+            <p class='sector-rating'>
+              <a href='#0'>17.67</a>
+            </p>
+          </li>
+
+          <li class='source'>Source: Refinitiv</li>
+
+          {/* end 15 */}
+
+          <li>
+            <p class='sector'>Price Performance</p>
+            <p class='sector-rating'>
+              <a href='#0'>Exchange-Nasdaq</a>
+            </p>
+          </li>
+          <li>
+            <p class='sector'>52 Week range</p>
+            <p class='sector-rating'>
+              <a href='#0'>USD 48.46-37.42</a>
+            </p>
+          </li>
+
+          <li class='eqchart'>
+            <img src='img/eqchart.png' alt='' />
+          </li>
+
+            <li class='source'>Link to Barclays Live for interactive charting</li>
+            </ul>
             <table style={{ border: "none" }}>
               <tr style={{ textAlign: "right" }}>
                 <button>FLT Page</button>
