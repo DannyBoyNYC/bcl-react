84ac530276c29dc89de53ba34f1825c514e9556b
DannyBoyNYC <daniel.deverell@gmail.com>
2019-06-17 13:19:52 -0400

    done

---
+++ html-renderer-webapp-client/src/main/webapp/src/body/components/PremierEvent.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class PremierEvent extends Component {
+  render() {
+    return (
+      <div class="premierevent">
+        <h3>Thursday, June 27, 2019</h3>
+        <h4>12:00pm - 2:00pm</h4>
+        <h4>1745 7th Avenue, New York</h4>
+        <button>Request Invitation</button>
+      </div>
+    );
+  }
+}
+
+export default PremierEvent;
